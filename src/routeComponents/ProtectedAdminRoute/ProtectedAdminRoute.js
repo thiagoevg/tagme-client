f98ca38751c5f300825b1ea561493f9d275204ff
Thiago Evangelista <thiagoevg@MacBook-Pro.local>
2021-06-04 18:40:46 -0300

    small bug in the url navigation fixed

--- src/routeComponents/ProtectedAdminRoute/ProtectedAdminRoute.js
+++ src/routeComponents/ProtectedAdminRoute/ProtectedAdminRoute.js
@@ -1,17 +1,18 @@
 import { Route, Redirect } from 'react-router-dom'
-import { useContext } from 'react'
-import { AuthContext } from '../../context/AuthContext'
 
 
 // Rota customizada protegida. Permite acesso ao componente requisitado apenas para o administrador.
 
 function ProtectedUserRoute({ component: Component, ...rest }) {
 
-  const { userInfo } = useContext(AuthContext)
+
+
+  const userInfo = JSON.parse(localStorage.getItem('userInfo') || `""`)
+
 
   return (
     <Route {...rest} render={(routeProps) => {
-      if (userInfo.user.role === "ADMIN") {
+      if (userInfo.user && userInfo.user.role === "ADMIN") {
         return (<Component {...routeProps} />)
       } else {
         return (<Redirect to={{ pathname: '/', state: { from: routeProps.location } }} />)
